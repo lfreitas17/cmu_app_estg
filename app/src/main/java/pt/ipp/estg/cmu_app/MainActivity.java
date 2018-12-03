05a405e08b62e73b9092f7d7c698da89a5d2a11e
nuno837 <nunofilmaia@yahoo.com.br>
2018-12-03 14:32:04 +0000

    Login draft
    Criação de um draft do login

--- app/src/main/java/pt/ipp/estg/cmu_app/MainActivity.java
+++ app/src/main/java/pt/ipp/estg/cmu_app/MainActivity.java
@@ -1,14 +1,55 @@
 package pt.ipp.estg.cmu_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = findViewById(R.id.etName);
+        Password = findViewById(R.id.etPassword);
+        Info = findViewById(R.id.tvInfo);
+        Login = findViewById(R.id.loginButton);
+
+        Info.setText("Nr of incorrect attempts: 0");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))){
+
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter++;
+
+            Info.setText("Nr of incorrect attempts:" + String.valueOf(counter));
+
+            if(counter == 10){
+                Login.setEnabled(false);
+            }
+        }
     }
 }
-//ola
